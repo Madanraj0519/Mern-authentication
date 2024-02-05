56b051cae42242ff998869fe3dc2a1e54b0297e7
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-06 00:04:40 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
+
+mongoose.connect(process.env.MONGO_DB).then(() => {
+    console.log('Database connection established successfully');
+}).catch((err) => { console.log(err); });
+
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
