dcd96b3d465ee8b08596a61adb9519c82a357bf2
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-15 03:04:33 +0530

    created a sign-in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require('../model/user.model');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const errorHandler = require('../utils/err');
 
 const signUp = async(req, res, next) => {
@@ -19,4 +20,30 @@ const signUp = async(req, res, next) => {
 };
 
 
+const signIn = async (req, res, next) => {
+   try{
+    const {userEmail, userPassword} = req.body;
+    const validUser = await userModel.findOne({userEmail});
+    if(!validUser){
+        return next(errorHandler(404, 'user not found'));
+    }
+    //comparing the password which is coming from the request and in the database
+    const validPassword = bcrypt.compareSync(userPassword, validUser.userPassword);
+    if(!validPassword){
+        return next(errorHandler(401, 'Invalid credentials'));
+    }
+    const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET);
+    /* Just hidding the password from the client side for the security purpose*/
+    const {userPassword : hashPassword, ...restDetails} = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res.cookie('access-token', token, {httpOnly: true, expires : expiryDate}).
+    status(200).
+    json(restDetails);
+   }catch(err){
+    next(err)
+   }
+};
+
+
 module.exports = signUp;
+module.exports = signIn;
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const signUp = require('../controller/auth.controller');
+const signIn = require('../controller/auth.controller');
 const authRouter = express.Router();
 
 authRouter.post('/signup', signUp);
+authRouter.post('/signin', signIn);
 
 module.exports = authRouter;
